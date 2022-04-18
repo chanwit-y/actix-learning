8b7a377244f5c1a166fea392bc19f4883c688bc6
chanwit-y <chanwit.yimneam@hotmail.com>
2022-04-18 13:10:59 +0700

    add state

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,23 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer};
+
+struct AppState {
+    app_name: String,
+}
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+    format!("Hello {app_name}")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix web"),
+            }))
+            .service(index)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
